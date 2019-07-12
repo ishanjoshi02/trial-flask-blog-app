c0f2b09617a4530a07a58cf6805fa1beff662f87
Ishan Joshi <ishanjoshi02@gmail.com>
2019-07-12 10:52:36 +0530

    changed connection url to amazon url

--- database_config.py
+++ database_config.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from config import DEBUG
 
-database_uri = "mysql://root:toor2019@localhost:3306/sys"
+database_uri = "mysql://root:toor2019@flaskdemoinstance.c0x127jnab22.ap-south-1.rds.amazonaws.com:3306/sys"
 engine = create_engine(database_uri)
 
 Session = sessionmaker()
@@ -14,8 +14,8 @@ session = Session()
 def getConfig():
     if DEBUG:
         return {
-            "HOST_NAME": 'localhost',
+            "HOST_NAME": 'flaskdemoinstance.c0x127jnab22.ap-south-1.rds.amazonaws.com',
             "USERNAME": "root",
-            "PASSWORD": "toor2019"
+            "PASSWORD": "toor2019",
             "DATABASE": "sys",
         }
