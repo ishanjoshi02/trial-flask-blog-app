5794f93230ba757aef3634b3700313d12e1983ad
Ishan Joshi <ishanjoshi02@gmail.com>
2019-07-12 08:10:20 +0530

    created getConfig function

--- database_config.py
+++ database_config.py
@@ -1,9 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from config import DEBUG
+
 database_uri = "mysql://root:toor2019@localhost:3306/sys"
 engine = create_engine(database_uri)
 
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
+
+
+def getConfig():
+    if DEBUG:
+        return {
+            "HOST_NAME": 'localhost',
+            "USERNAME": "root",
+            "PASSWORD": "toor2019"
+            "DATABASE": "sys",
+        }
