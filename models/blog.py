04fc456170265bf5b43647c7416a4d775dbe2d77
Ishan Joshi <ishanjoshi02@gmail.com>
2019-07-12 08:08:43 +0530

    changed table name and blog_id to id

--- models/blog.py
+++ models/blog.py
@@ -1,17 +1,30 @@
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, TIMESTAMP
 
 Base = declarative_base()
 
+
 class BlogModel(Base):
 
-    __tablename__ = "blogs"
+    __tablename__ = "blog_posts"
 
-    blog_id = Column(Integer, primary_key=True)
-    title = Column(String)
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
     content = Column(String)
     author = Column(Integer)
+    created_on = Column(TIMESTAMP)
+    deleted_on = Column(TIMESTAMP)
+    edited_on = Column(TIMESTAMP)
+
+    def getDict(self):
+        d = {
+            'id': self.id,
+            "name": self.name,
+            "content": self.content,
+            "author": self.author
+        }
+        return d
 
     def __repr__(self):
-        return "<BlogModel title: {} AuthorId: {} >".format(self.title, self.author)
+        return "<BlogModel title: {} AuthorId: {} >".format(self.name, self.author)
