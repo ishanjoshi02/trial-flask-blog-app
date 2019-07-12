0af53f13c4d3f23238ec82dd839a8db23b992303
Ishan Joshi <ishanjoshi02@gmail.com>
2019-07-12 11:54:11 +0530

    uploading to ec2

--- app.py
+++ app.py
@@ -277,4 +277,4 @@ api.add_resource(BlogList, '/blog/<int:start>/<int:end>')
 api.add_resource(Blog, '/blog/<int:id>')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80)
---
+++ application.py
@@ -0,0 +1,7 @@
+# import subprocess
+
+# subprocess.call("FLASK_APP=/app.py FLASK_DEBUG=0 python3 - m flask run")
+
+import os
+
+os.system("FLASK_APP=app.py FLASK_DEBUG=1 python3 - m flask run")
