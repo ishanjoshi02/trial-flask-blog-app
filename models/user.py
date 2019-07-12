2f4bec28d6177154d8790486543ba1f130e10337
Ishan Joshi <ishanjoshi02@gmail.com>
2019-07-12 08:09:02 +0530

    changed table name and user_id to id

--- models/user.py
+++ models/user.py
@@ -4,21 +4,22 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
-class UserModel(Base):
-    __tablename__ = "user"
 
-    user_id = Column(Integer, primary_key=True)
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     username = Column(String)
     password = Column(String)
 
-    is_authenticated=True
-    is_active=True
-    is_anonymous=True
+    is_authenticated = True
+    is_active = True
+    is_anonymous = False
 
     def get_id(self):
-       
-        return str(self.user_id).encode("utf-8").decode("utf-8")
+
+        return str(self.id).encode("utf-8").decode("utf-8")
 
     def __repr__(self):
         return "<UserModel username: {} name:{}>".format(self.username, self.name)
